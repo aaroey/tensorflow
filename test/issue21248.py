83e4c23d4ebe505798859e33f4a40db4b4297584
gracehoney <31743510+aaroey@users.noreply.github.com>
2018-08-09 11:23:31 -0700

    add more debug infor for issue21248.py

--- test/issue21248.py
+++ test/issue21248.py
@@ -1,6 +1,9 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.contrib import tensorrt as trt
+from tensorflow.core.protobuf import meta_graph_pb2
+from tensorflow.core.protobuf import rewriter_config_pb2
+from tensorflow.python.grappler import tf_optimizer
 
 
 def build_graph_from_def(graph_def, input_nodes, output_nodes):
@@ -22,26 +25,49 @@ def build_graph_from_def(graph_def, input_nodes, output_nodes):
 
 
 def main():
-  with tf.variable_scope("Net"):
-    inp = tf.placeholder(tf.float32, shape=(1, 28, 28, 3), name="input_image")
-    deconv1 = tf.layers.conv2d_transpose(
-        inp, filters=8, kernel_size=(3, 3), strides=(2, 2))
-    output = tf.layers.conv2d(
-        deconv1, filters=8, kernel_size=(3, 3), name="output")
-  input_nodes = ["Net/input_image"]
-  output_nodes = ["Net/output/BiasAdd"]
+  inp = tf.placeholder(tf.float32, shape=(1, 28, 28, 3), name="input_image")
+  deconv1 = tf.layers.conv2d_transpose(
+      inp, filters=8, kernel_size=(3, 3), strides=(2, 2))
+  output = tf.layers.conv2d(
+      deconv1, filters=8, kernel_size=(3, 3), name="output")
+
+  input_nodes = ["input_image"]
+  output_nodes = ["output/BiasAdd"]
   with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     const_graph_def = tf.graph_util.convert_variables_to_constants(
         sess, sess.graph.as_graph_def(), output_nodes)
-  tf.train.write_graph(const_graph_def, "/tmp", "issue21248.const.pbtxt")
+  tf.train.write_graph(const_graph_def, "/tmp/lambdatfpy",
+                       "issue21248.const.pbtxt")
+
+  # Run const folding manually
+  graph = tf.Graph()
+  with graph.as_default():
+    tf.import_graph_def(const_graph_def, name="")
+  meta_graph = tf.train.export_meta_graph(
+      graph_def=graph.as_graph_def(), graph=graph)
+  output_collection = meta_graph_pb2.CollectionDef()
+  output_list = output_collection.node_list.value
+  to_bytes = lambda inp: inp.encode("utf-8", errors="surrogateescape")
+  for i in output_nodes:
+    if isinstance(i, tf.Tensor):
+      output_list.append(to_bytes(i.name))
+    else:
+      output_list.append(to_bytes(i))
+  meta_graph.collection_def["train_op"].CopyFrom(output_collection)
+  rewriter_cfg = rewriter_config_pb2.RewriterConfig()
+  rewriter_cfg.optimizers.extend(["constfold", "layout"])
+  folded_gdef = tf_optimizer.OptimizeGraph(
+      rewriter_cfg, meta_graph, graph_id=b"tf_graph")
+  tf.train.write_graph(folded_gdef, "/tmp/lambdatfpy",
+                       "issue21248.folded.pbtxt")
 
   optimized_graph_def = trt.create_inference_graph(
-      input_graph_def=const_graph_def,
+      input_graph_def=folded_gdef,
       outputs=output_nodes,
       max_batch_size=1,
       max_workspace_size_bytes=1 << 25)
-  tf.train.write_graph(optimized_graph_def, "/tmp",
+  tf.train.write_graph(optimized_graph_def, "/tmp/lambdatfpy",
                        "issue21248.optimized.pbtxt")
   graph, input_tensors, output_tensors = build_graph_from_def(
       optimized_graph_def, input_nodes, output_nodes)
