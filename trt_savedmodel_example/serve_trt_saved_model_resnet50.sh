3f5b17518ae6ce74983fff432ffcc6b3270d442f
Guangda Lai <31743510+aaroey@users.noreply.github.com>
2018-10-06 22:40:46 -0700

    Fix trt saved model serving script.

--- trt_savedmodel_example/serve_trt_saved_model_resnet50.sh
+++ trt_savedmodel_example/serve_trt_saved_model_resnet50.sh
@@ -2,15 +2,13 @@
 set -ex
 
 # Prerequisites: install tensorflow with version >=1.12
-work_dir=/tmp/trt_saved_model
-mkdir -p $work_dir
-cd $work_dir
+WORK_DIR=/tmp/trt_saved_model
 
-if [[ "$1" == "server" ]]; then
-  saved_model_name=20180601_resnet_v2_imagenet_savedmodel
-  saved_model_url=http://download.tensorflow.org/models/official/$saved_model_name.tar.gz
-  saved_model_path=$work_dir/$saved_model_name
-  trt_saved_model_path=${saved_model_path}_trt
+run_server() {
+  local saved_model_name=20180601_resnet_v2_imagenet_savedmodel
+  local saved_model_url=http://download.tensorflow.org/models/official/$saved_model_name.tar.gz
+  local saved_model_path=$WORK_DIR/$saved_model_name
+  local trt_saved_model_path=${saved_model_path}_trt
 
   if ! [[ -d $saved_model_path ]]; then
     curl -O $saved_model_url
@@ -18,7 +16,7 @@ if [[ "$1" == "server" ]]; then
   fi
 
   if ! [[ -f $trt_saved_model_path/1/saved_model.pb ]]; then
-    saved_model_path_with_version="$(echo $saved_model_path/*)"
+    local saved_model_path_with_version="$(echo $saved_model_path/*)"
     python <<< "
 import tensorflow.contrib.tensorrt as trt
 trt.create_inference_graph(
@@ -30,19 +28,54 @@ trt.create_inference_graph(
 "
   fi
 
+  local tag=''
+  if [[ "$1" == 'local' ]]; then
+    curl -O https://raw.githubusercontent.com/tensorflow/serving/master/tensorflow_serving/tools/docker/Dockerfile.devel-gpu
+    curl -O https://raw.githubusercontent.com/tensorflow/serving/master/tensorflow_serving/tools/docker/Dockerfile.gpu
+    docker build --pull -t my-tf-serving-trt-tf-head-env \
+      -f Dockerfile.devel-gpu \
+      .
+    docker build -t my-tf-serving-trt-devel \
+      --build-arg TF_SERVING_BUILD_IMAGE=my-tf-serving-trt-tf-head-env \
+      -f Dockerfile.gpu \
+      .
+    tag=my-tf-serving-trt-devel
+  else
+    tag=tensorflow/serving:nightly-gpu
+    # tag=tensorflow/serving:latest-gpu
+    docker pull $tag
+  fi
+
   # NOTE: 8500 is GRPC port, 8501 is REST port, the -p option binds the docker
   # port with host port, if not bind we'll not be able to access that port from
   # host.
-  docker run --runtime=nvidia -p 8500:8500 \
+  #
+  # To debug the model server, add `--cap-add=SYS_PTRACE` to the `docker run`
+  # command, and attach to it via `docker exec -i -t <container id> /bin/bash`
+  # and then `gdb`.
+  docker run --cap-add=SYS_PTRACE --runtime=nvidia -p 8500:8500 \
     --mount type=bind,source="$trt_saved_model_path",target=/models/mymodel \
-    -e MODEL_NAME=mymodel -t \
-    tensorflow/serving:nightly-gpu &
-    # tensorflow/serving:latest-gpu &
-elif [[ "$1" == "client" ]]; then
+    -e MODEL_NAME=mymodel -t $tag &
+}
+
+run_client() {
   curl -O https://raw.githubusercontent.com/aaroey/tensorflow/trt_savedmodel_example/trt_savedmodel_example/serve_trt_saved_model_resnet50_client.py
   python serve_trt_saved_model_resnet50_client.py \
     --num_tests=10 \
     --server=127.0.0.1:8500
+}
+
+mkdir -p $WORK_DIR
+cd $WORK_DIR
+mode="$1"
+shift
+
+if [[ "$mode" == "server" ]]; then
+  run_server "$@"
+elif [[ "$mode" == "client" ]]; then
+  run_client "$@"
+elif [[ "$mode" == "clean" ]]; then
+  rm -rf $WORK_DIR
 else
-  echo 'Usage: run_oss.sh server|client'
+  echo "Usage: serve_trt_saved_model_resnet50.sh server|client|clean"
 fi
