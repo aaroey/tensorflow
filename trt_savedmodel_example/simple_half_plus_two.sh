94b7acdf6e0fffbff7f70ef83251dd0602c043d9
Guangda Lai <31743510+aaroey@users.noreply.github.com>
2019-01-14 17:41:03 -0800

    use docker everywhere

--- trt_savedmodel_example/simple_half_plus_two.sh
+++ trt_savedmodel_example/simple_half_plus_two.sh
@@ -3,33 +3,38 @@ set -ex
 
 # Prerequisites: install tensorflow with version >=1.12
 WORK_DIR=/tmp/trt_saved_model_simple_half_plus_two
-rm -rf $WORK_DIR
 
 run_server() {
+  rm -rf $WORK_DIR/*
   local saved_model_path=$WORK_DIR/saved_model
   local trt_saved_model_path=$WORK_DIR/saved_model_trt
   mkdir -p $saved_model_path $trt_saved_model_path
-  rm -rf $saved_model_path $trt_saved_model_path
 
   curl -O \
     https://raw.githubusercontent.com/aaroey/tensorflow/trt_savedmodel_example/trt_savedmodel_example/simple_half_plus_two.py
 
-  # TF-Serving needs the version directory, so we hard-coded version 1 here.
-  python simple_half_plus_two.py     \
-    --output_dir=$saved_model_path/1 \
-    --output_dir_trt=$trt_saved_model_path/1
+  # Change to tensorflow/tensorflow:latest-gpu whenever appropriate.
+  local tf_tag=tensorflow/tensorflow:1.12.0-gpu
+  docker pull $tf_tag
+  docker run --runtime=nvidia --rm -v $WORK_DIR:$WORK_DIR -t $tf_tag \
+    python $WORK_DIR/simple_half_plus_two.py --output_dir=$saved_model_path/1 --output_dir_trt=$trt_saved_model_path/1
+  # TF-Serving needs the version directory, so we hard-coded version 1 above.
 
-  local tag=tensorflow/serving:latest-gpu
-  docker pull $tag
-  docker run --runtime=nvidia -p 8500:8500 \
-    --mount type=bind,source="$trt_saved_model_path",target=/models/mymodel \
-    -e MODEL_NAME=mymodel -t $tag &
+  # Change to tensorflow/serving:latest-gpu whenever appropriate.
+  local tfs_tag=tensorflow/serving:1.12.0-gpu
+  docker pull $tfs_tag
+  docker run --runtime=nvidia --rm -p 8500:8500 \
+    -v $trt_saved_model_path:/models/mymodel \
+    -e MODEL_NAME=mymodel -t $tfs_tag &
 }
 
 run_client() {
   curl -O \
     https://raw.githubusercontent.com/aaroey/tensorflow/trt_savedmodel_example/trt_savedmodel_example/simple_half_plus_two_client.py
-  python simple_half_plus_two_client.py
+  local tfs_tag=tensorflow/serving:1.12.0-devel-gpu
+  docker pull $tfs_tag
+  docker run --network host --runtime=nvidia --rm -v $WORK_DIR:$WORK_DIR -t $tfs_tag \
+    python $WORK_DIR/simple_half_plus_two_client.py
 }
 
 mkdir -p $WORK_DIR
